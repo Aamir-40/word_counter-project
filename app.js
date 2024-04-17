8b6f5e4fce0673bdd60cef49777eac379433833f
aamir <s.aamirali91@example.com>
2024-04-17 18:36:35 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([{
+        name: "sentence",
+        type: "input",
+        message: "Enter a sentence to count the words you want to count :"
+    }
+]);
+const words = answer.sentence.trim().split(" ");
+console.log(words);
+console.log(`The number of words in the sentence is : ${words.length}`);
